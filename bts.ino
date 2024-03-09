3e79324c5c53458d0b91d2aa0790f4218e10ab09
AditLuthra <39406446+AditLuthra@users.noreply.github.com>
2024-03-09 13:01:50 +0530

    Update bts.ino

--- bts.ino
+++ bts.ino
@@ -1,18 +1,18 @@
-//Receiver signal pins
+// Receiver signal pins
 int vx = A0;  // Analog pin for joystick x-axis
 int vy = A1;  // Analog pin for joystick y-axis
 
-//Right motor driver pins
+// Right motor driver pins
 int R_EN_right = 2; 
 int L_EN_right = 4;
-int R_PWM_right = 6; //PWM pin
-int L_PWM_right = 9; //PWM pin
+int R_PWM_right = 6; // PWM pin
+int L_PWM_right = 9; // PWM pin
 
-//Left motor driver pins
+// Left motor driver pins
 int R_EN_left = 7; 
 int L_EN_left = 8;
-int R_PWM_left = 10; //PWM pin
-int L_PWM_left = 11; //PWM pin
+int R_PWM_left = 10; // PWM pin
+int L_PWM_left = 11; // PWM pin
 
 void setup()
 {
@@ -31,80 +31,77 @@ void setup()
 
 void loop()
 {
-  
   int x = analogRead(vx);
   int y = analogRead(vy);
 
   Serial.print("X ");
   Serial.println(x);
   Serial.print("\t");
-  Serial.print("Y");
-  Serial.print(y);
-  Serial.println("");
-  if(x>507){
-    if(y>523){
-      out1=map(x,507,1023,0,255)
-      out2=map(y,523,1023,0,255)
-      sub=abs(out2-out1)
-      add=out2+out1
-      analogWrite(R_PWM_right,sub);
-      analogWrite(R_PWM_left,add);
-      analogWrite(L_PWM_right,0);
-      analogWrite(L_PWM_left,0);
-    }
-    else if(y<523){
-      out1=map(x,507,1023,0,255)
-      out2=map(y,523,0,255)
-      sub=abs(out2-out1)
-      add=out2+out1
-      analogWrite(R_PWM_right,add);
-      analogWrite(R_PWM_left,sub);
-      analogWrite(L_PWM_right,0);
-      analogWrite(L_PWM_left,0);
-
-    }
-    else{
-      out1=map(x,507,1023,0,255)
-      analogWrite(R_PWM_right,out1);
-      analogWrite(R_PWM_left,out1);
-      analogWrite(L_PWM_right,0);
-      analogWrite(L_PWM_left,0);
-    }
-  }
-  elseif(x<507){
-    if(y>523){
-      out1=map(x,507,0,0,255)
-      out2=map(y,523,1023,0,255)
-      sub=abs(out2-out1)
-      add=out2+out1
-      analogWrite(L_PWM_right,sub);
-      analogWrite(L_PWM_left,add);
-      analogWrite(R_PWM_right,0);
-      analogWrite(R_PWM_left,0);
-    }
-    else if(y<523){
-      out1=map(x,507,0,0,255)
-      out2=map(y,523,0,255)
-      sub=abs(out2-out1)
-      add=out2+out1
-      analogWrite(L_PWM_right,add);
-      analogWrite(L_PWM_left,sub);
-      analogWrite(R_PWM_right,0);
-      analogWrite(R_PWM_left,0);
-
-    }
-    else{
-      out1=map(x,507,0,0,255)
-      analogWrite(L_PWM_right,out1);
-      analogWrite(L_PWM_left,out1);
-      analogWrite(R_PWM_right,0);
-      analogWrite(R_PWM_left,0);
-    }
-  }
-  else{
-    analogWrite(R_PWM_right,0);
-    analogWrite(R_PWM_left,0);
-    analogWrite(L_PWM_right,0);
-    analogWrite(L_PWM_left,0);
-  }
+  Serial.print("Y ");
+  Serial.println(y);
   
+  if (x > 507) {
+    if (y > 523) {
+      int out1 = map(x, 507, 1023, 0, 255);
+      int out2 = map(y, 523, 1023, 0, 255);
+      int sub = abs(out2 - out1);
+      int add = out2 + out1;
+      analogWrite(R_PWM_right, sub);
+      analogWrite(R_PWM_left, add);
+      analogWrite(L_PWM_right, 0);
+      analogWrite(L_PWM_left, 0);
+    }
+    else if (y < 523) {
+      int out1 = map(x, 507, 1023, 0, 255);
+      int out2 = map(y, 523, 0, 255);
+      int sub = abs(out2 - out1);
+      int add = out2 + out1;
+      analogWrite(R_PWM_right, add);
+      analogWrite(R_PWM_left, sub);
+      analogWrite(L_PWM_right, 0);
+      analogWrite(L_PWM_left, 0);
+    }
+    else {
+      int out1 = map(x, 507, 1023, 0, 255);
+      analogWrite(R_PWM_right, out1);
+      analogWrite(R_PWM_left, out1);
+      analogWrite(L_PWM_right, 0);
+      analogWrite(L_PWM_left, 0);
+    }
+  }
+  else if (x < 507) {
+    if (y > 523) {
+      int out1 = map(x, 507, 0, 0, 255);
+      int out2 = map(y, 523, 1023, 0, 255);
+      int sub = abs(out2 - out1);
+      int add = out2 + out1;
+      analogWrite(L_PWM_right, sub);
+      analogWrite(L_PWM_left, add);
+      analogWrite(R_PWM_right, 0);
+      analogWrite(R_PWM_left, 0);
+    }
+    else if (y < 523) {
+      int out1 = map(x, 507, 0, 0, 255);
+      int out2 = map(y, 523, 0, 255);
+      int sub = abs(out2 - out1);
+      int add = out2 + out1;
+      analogWrite(L_PWM_right, add);
+      analogWrite(L_PWM_left, sub);
+      analogWrite(R_PWM_right, 0);
+      analogWrite(R_PWM_left, 0);
+    }
+    else {
+      int out1 = map(x, 507, 0, 0, 255);
+      analogWrite(L_PWM_right, out1);
+      analogWrite(L_PWM_left, out1);
+      analogWrite(R_PWM_right, 0);
+      analogWrite(R_PWM_left, 0);
+    }
+  }
+  else {
+    analogWrite(R_PWM_right, 0);
+    analogWrite(R_PWM_left, 0);
+    analogWrite(L_PWM_right, 0);
+    analogWrite(L_PWM_left, 0);
+  }
+}
